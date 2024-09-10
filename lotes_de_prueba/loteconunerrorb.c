36f2755149be454fa3ee11a88cc3d0d3ebf2f140
Agusrqlm <agustinrequelme4@gmail.com>
2024-09-09 22:50:28 -0300

    Algunos lotes de prueba

---
+++ lotes_de_prueba/loteconunerrorb.c
@@ -0,0 +1,9 @@
+in main() {
+    int cont = 0;
+    while (cont < 10) {
+        cout << "Cont: " << count << "";
+    }
+        if (cont == 0 {
+            cout << "Count es par." << "";
+        }
+}
---
+++ lotes_de_prueba/lotesinerrores.c
@@ -0,0 +1,45 @@
+int fedad() {
+    int edad;
+
+    cout << "Ingresa tu edad: ";
+    cin >> edad;
+    if (edad >= 18) {
+        cout << "Mayor de edad.\n";
+    } else {
+        cout << "Menor de edad.\n";
+    }
+    return 0;
+}
+
+int fnum() {
+    int contador = 1;
+    while (contador <= 5) {
+        cout << "Número: " << contador;
+        contador= contador + 1;
+    }
+    return 0;
+}
+
+char letra;         
+char nombre[50];  
+float decimal = 3.14; 
+   
+int fsuma() {
+    int num1;
+    int num2;
+    fmostrar(num1, num2);
+    if ((j < 5) && (k > 10)) {
+        fsuma();
+        return 1;
+    }       
+    else{
+    while ((i < 5) && (j > 0) || (k != 0)) {
+        fmostrar(num1, num2);
+    }
+        return 0;
+    }
+}
+
+void fhola(int algo){
+    int a;
+}
